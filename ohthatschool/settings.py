73d5484ea4d19d9b336f083b6af843bb04cc6bf9
Marosek88 <marek.kubiak88@gmail.com>
2020-04-01 13:10:16 +0100

    Settings adjusted to production

--- ohthatschool/settings.py
+++ ohthatschool/settings.py
@@ -22,9 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DEBUG') == "True":
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://ohthatschool-react.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'ohthatschool-react.herokuapp.com']
 
 # Application definition
 
