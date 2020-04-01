49d7629feea94da82198bd1d12b25dfa428f8540
Marosek88 <marek.kubiak88@gmail.com>
2020-04-01 13:03:40 +0100

    Procfile added

--- ohthatschool/settings.py
+++ ohthatschool/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'https://ohthatschool-react.herokuapp.com/']
 
 # Application definition
 
